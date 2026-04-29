db34d6445b89d69fdc377e4af8eeaadcaa9f34bc
Reisa <joaquinzacharydorongon@gmail.com>
2026-04-30 00:24:44 +0800

    Fixed the comments from quiz 1. Added DeductStock() and added separate class for Product

---
+++ ShoppingCartActivity/Product.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartActivity
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public int RemainingStock { get; set; }
+
+        public Product(int id, string name, double price, int remainingStock)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            RemainingStock = remainingStock;
+        }
+        public void DisplayProduct()
+        {
+            Console.WriteLine($"{Id,-3} {Name,-10} {Price,8:F2} {RemainingStock,8}");
+        }
+        public bool HasEnoughStock(int quantity)
+        {
+            return RemainingStock >= quantity;
+        }
+        public void DeductStock(int quantity)
+        {
+            RemainingStock -= quantity;
+        }
+    }
+
+    class CartItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public double Subtotal { get; set; }
+
+        public CartItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+            Subtotal = product.Price * quantity;
+        }
+
+        public void Update(int quantity)
+        {
+            Quantity += quantity;
+            Subtotal = Product.Price * Quantity;
+        }
+        public void DisplayCartItem()
+        {
+            Console.WriteLine($"{Product.Name,-10} {Quantity,-5} ${Subtotal:F2}");
+        }
+    }
+}
--- ShoppingCartActivity/Program.cs
+++ ShoppingCartActivity/Program.cs
@@ -7,55 +7,6 @@ using System.Threading.Tasks;
 
 namespace ShoppingCartActivity
 {
-    class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Price { get; set; }
-
-        public int RemainingStock { get; set; }
-
-        public Product(int id, string name, double price, int remainingStock)
-        {
-            Id = id;
-            Name = name;
-            Price = price;
-            RemainingStock = remainingStock;
-        }
-        public void DisplayProduct()
-        {
-            Console.WriteLine($"{Id,-3} {Name,-10} {Price,8:F2} {RemainingStock,8}");
-        }
-        public bool HasEnoughStock(int quantity)
-        {
-            return RemainingStock >= quantity;
-        }
-    }
-
-    class CartItem
-    {
-        public Product Product { get; set; }
-        public int Quantity { get; set; }
-        public double Subtotal { get; set; }
-
-        public CartItem(Product product, int quantity)
-        {
-            Product = product;
-            Quantity = quantity;
-            Subtotal = product.Price * quantity;
-        }
-
-        public void Update(int quantity)
-        {
-            Quantity += quantity;
-            Subtotal = Product.Price * Quantity;
-        }
-        public void DisplayCartItem()
-        {
-            Console.WriteLine($"{Product.Name,-10} {Quantity,-5} ${Subtotal:F2}");
-        }
-    }
-
     class Program
     {
         static void DisplayMenu(Product[] products)
@@ -175,7 +126,7 @@ namespace ShoppingCartActivity
                                 cartCount++;
                             }
                             cartQuantity += quantity;
-                            selectedProd.RemainingStock -= quantity;
+                            selectedProd.DeductStock(quantity);
                             Console.WriteLine("Item added to cart.\n");
                         }
                     }
